a0c9cadcc903d86a690bdc2759764c6faf9ee394
Guo, Yong Kang <yong.kang.guo@sap.com>
2024-04-03 15:32:06 +0800

    simple test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Go to game start/i);
-  expect(linkElement).toBeInTheDocument();
+  const titlElement = screen.getByText(/othello/i);
+  expect(titlElement).toBeInTheDocument();
 });
+
+test('render who\'s turn', () => {
+  render(<App />);
+  const whosTurnElement = screen.getByText(/.*'s turn/i);
+  expect(whosTurnElement).toBeInTheDocument();
+})
+
+test('game status', () => {
+  render(<App />);
+  const blackStone = screen.getByText(/blacks:\s*\d+/i);
+  expect(blackStone).toBeInTheDocument();
+  const whiteStone = screen.getByText(/whites:\s*\d+/i);
+  expect(whiteStone).toBeInTheDocument();
+})
